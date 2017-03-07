6ebb4218a61ca753a75d5e89397645ac7f44593b
daymis <daymis.vargas@gmail.com>
2017-03-06 17:18:07 -0800

    Add files via upload

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -2,40 +2,57 @@ import media
 import fresh_tomatoes
 
 #my favorite movies, in no particular order
+#each movie object needs the movie title, a description, its
+#poster image's url and the trailer url to be instantiated
+
 
 #Hidden Figures
 hidden_figures = media.Movie("Hidden Figures",
-"The incredible untold story of Katherine G. Johnson, Dorothy Vaughan and Mary Jackson - brilliant African-American women working at NASA.",
+"The incredible untold story of Katherine G. Johnson, Dorothy"
+" Vaughan and Mary Jackson - brilliant African-American women"
+" working at NASA.",
 "http://t1.gstatic.com/images?q=tbn:ANd9GcRqU2FbN7Zp0ELl-sX4lO8XO0pTjpmdJcpos_fnP9wM9DQHYgFq",
 "https://www.youtube.com/watch?v=5wfrDhgUMGI")
 
 #Split
 split = media.Movie("Split",
-"Compelled to abduct three teenage girls, Kevin reaches a war for survival among all of those contained within him - as well as everyone around him - as the walls between his compartments shatter apart.",
+"Compelled to abduct three teenage girls, Kevin reaches a war"
+" for survival among all of those contained within him - as well"
+" as everyone around him - as the walls between his compartments shatter apart.",
 "http://t0.gstatic.com/images?q=tbn:ANd9GcQxZKPueKzPFNEc1Un4x2TecYop4yrTVArVtfgrNzktMqbfehfv",
 "https://www.youtube.com/watch?v=84TouqfIsiI")
 
 #Get Out
 get_out = media.Movie("Get Out",
-"At first, Chris reads his girlfriend's family's overly accommodating behavior as nervous attempts to deal with their daughter's interracial relationship, but as the weekend progresses, a series of increasingly disturbing discoveries lead him to a truth that he never could have imagined.",
+"At first, Chris reads his girlfriend's family's overly"
+" accommodating behavior as nervous attempts to deal with their"
+" daughter's interracial relationship, but as the weekend"
+" progresses, a series of increasingly disturbing discoveries"
+" lead him to a truth that he never could have imagined.",
 "https://images-na.ssl-images-amazon.com/images/M/MV5BNTE2Nzg1NjkzNV5BMl5BanBnXkFtZTgwOTgyODMyMTI@._V1_SY1000_CR0,0,631,1000_AL_.jpg",
 "https://www.youtube.com/watch?v=sRfnevzM9kQ")
 
 #Pride and Prejudice
 pride_and_prejudice = media.Movie("Pride and Prejudice",
-"Pride & Prejudice is a romantic drama based on Jane Austen's 1813 novel of the same name.",
+"Pride & Prejudice is a romantic drama based on Jane Austen's"
+" 1813 novel of the same name.",
 "http://img.moviepostershop.com/pride-and-prejudice-movie-poster-2005-1020293519.jpg",
 "https://www.youtube.com/watch?v=1dYv5u6v55Y")
 
 #Black Swan
 black_swan = media.Movie("Black Swan",
-"Swan Lake requires a dancer who can play both the White Swan with innocence and grace, and the Black Swan, who represents guile and sensuality. Nina fits the White Swan role perfectly but Lily is the personification of the Black Swan.",
+"Swan Lake requires a dancer who can play both the White Swan"
+" with innocence and grace, and the Black Swan, who represents"
+" guile and sensuality. Nina fits the White Swan role perfectly"
+" but Lily is the personification of the Black Swan.",
 "http://t0.gstatic.com/images?q=tbn:ANd9GcQTEDENWuG9VwlAT8yRL_IkTYABMMmGmrQKcN8qjBLZYVkMOgWy",
 "https://www.youtube.com/watch?v=5jaI1XOB-bs")
 
 #Pineapple Express
 pineapple_express = media.Movie("Pineapple Express",
-"Lazy stoner Dale Denton has only one reason to visit his equally lazy dealer Saul Silver: to purchase weed, specifically, a rare new strain called Pineapple Express.",
+"Lazy stoner Dale Denton has only one reason to visit his"
+" equally lazy dealer Saul Silver: to purchase weed,"
+" specifically, a rare new strain called Pineapple Express.",
 "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY1MTE4NzAwM15BMl5BanBnXkFtZTcwNzg3Mjg2MQ@@._V1_.jpg",
 "https://www.youtube.com/watch?v=sW67OiY7xVw")
 
